a7c15a1b80c177f97decd579fb7025da24aed8c8
Peter Wagner <peter.wagner1986@gmail.com>
2024-03-05 19:27:29 +0100

    added person class

---
+++ js/classes/person.js
@@ -0,0 +1,17 @@
+export default class Person {
+    constructor(canvas, gender, age, job) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.setContext('2d');
+        this.gender = gender;
+        this.age = age;
+        this.job = job;
+    }
+
+    setPerson() {
+
+    }
+
+    drawPerson() {
+        
+    }
+}
